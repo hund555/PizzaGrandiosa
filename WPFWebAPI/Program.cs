2177a9b3ea82474d054b6ccbbc94ef80a84ac070
Sebastian Krull Greve <Skgreve@gmail.com>
2026-04-24 08:38:52 +0200

    Revert "Try"
    This reverts commit 55853a5312a08cb6a3f5aed5fcf4a44ca221ace6.

--- WPFWebAPI/Program.cs
+++ WPFWebAPI/Program.cs
@@ -10,15 +10,6 @@ namespace WPFWebAPI
             // Add services to the container.
             builder.Services.AddAuthorization();
 
-            // Http client used to call back the main API when responding to orders
-            builder.Services.AddHttpClient("Api", client => {
-                var apiBase = builder.Configuration["ApiBaseUrl"] ?? "http://localhost:5000";
-                client.BaseAddress = new Uri(apiBase);
-            });
-
-            // Register Rabbit subscriber service
-            builder.Services.AddSingleton<Services.RabbitSubscriber>();
-
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
@@ -33,34 +24,6 @@ namespace WPFWebAPI
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
-            // Subscriber endpoints to control subscription and respond to orders
-            var subGroup = app.MapGroup("/api/subscriber").WithTags("subscriber");
-
-            subGroup.MapPost("/subscribe", (Services.RabbitSubscriber subscriber) =>
-            {
-                subscriber.Subscribe();
-                return Results.Ok(new { subscribed = true });
-            });
-
-            subGroup.MapPost("/unsubscribe", (Services.RabbitSubscriber subscriber) =>
-            {
-                subscriber.Unsubscribe();
-                return Results.Ok(new { subscribed = false });
-            });
-
-            subGroup.MapGet("/pending", (Services.RabbitSubscriber subscriber) =>
-            {
-                var pending = subscriber.GetPendingOrders();
-                return Results.Ok(pending);
-            });
-
-            subGroup.MapPost("/respond", async (int orderId, bool accepted, Services.RabbitSubscriber subscriber) =>
-            {
-                var ok = await subscriber.RespondAsync(orderId, accepted);
-                if (!ok) return Results.NotFound(new { orderId });
-                return Results.Ok(new { orderId, accepted });
-            });
 
             
 
